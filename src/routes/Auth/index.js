0b6979c746e74172a1b45ef91305ac5a59399a1d
Jack Zhou <377738226@qq.com>
2018-08-04 02:19:40 +0800

    with redux alone -- no react-redux

--- src/routes/Auth/index.js
+++ src/routes/Auth/index.js
@@ -3,14 +3,14 @@ import Login from "components/Login";
 import { withRouter } from "react-router-dom";
 import { store } from "models/store";
 
-export default class Auth extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// export default class Auth extends React.Component {
+//   render() {
+//     const Wlogin = withRouter(Login);
+//     return <Wlogin store={store} />;
+//   }
+// }
 
-  render() {
-    console.info("withRouter: ", withRouter);
-    const Wlogin = withRouter(Login);
-    return <Wlogin store={store} />;
-  }
+export default function Auth() {
+  const Wlogin = withRouter(Login);
+  return <Wlogin store={store} />;
 }
