1e93e2fe36d9919f30ceeeba8f638d9f366d5e32
Jack Zhou <377738226@qq.com>
2018-08-04 11:51:17 +0800

    use store.subscribe to subscribe store changes and use this.setState to trigger Component render

--- src/app.js
+++ src/app.js
@@ -10,44 +10,63 @@ if (module.hot) {
   module.hot.accept();
 }
 
-function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route
-            path="/login"
-            render={() =>
-              store.getState().auth.login ? (
-                "welcome"
-              ) : (
-                <div className="login">
-                  <Auth store={store} />
-                </div>
-              )
-            }
-          />
-          <Route
-            path="/"
-            render={() => {
-              const {
-                auth: { login }
-              } = store.getState();
-              console.info("state updated! login: ", login);
-              const element = store.getState().auth.login ? (
-                <span>welcome</span>
-              ) : (
-                <div className="login">
-                  <Auth store={store} login={login} />
-                </div>
-              );
-              return element;
-            }}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const { store } = this.props;
+    this.state = store.getState();
+    const that = this;
+    store.subscribe(() => {
+      const {
+        auth: { login }
+      } = store.getState();
+      that.setState(() => ({
+        auth: {
+          login
+        }
+      }));
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <Switch>
+            <Route
+              path="/login"
+              render={() =>
+                store.getState().auth.login ? (
+                  "welcome"
+                ) : (
+                  <div className="login">
+                    <Auth store={store} />
+                  </div>
+                )
+              }
+            />
+            <Route
+              path="/"
+              render={() => {
+                const {
+                  auth: { login }
+                } = store.getState();
+                console.info("state updated! login: ", login);
+                const element = store.getState().auth.login ? (
+                  <span>welcome</span>
+                ) : (
+                  <div className="login">
+                    <Auth store={store} login={login} />
+                  </div>
+                );
+                return element;
+              }}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<App store={store} />, document.getElementById("root"));
