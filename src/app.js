58e96c0082bd2074ff60229a69671f94a594d4dc
anonymous0374 <377738226@qq.com>
2018-09-08 15:41:35 +0800

    remove unnecessary lines

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './app.less';
 
-// to enable module hot reload(instead of page reload)
-if (module.hot) {
-  module.hot.accept();
-}
-
 class App extends Component {
   render() {
     return (
