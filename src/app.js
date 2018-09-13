b58a2f9962f64b217ae8f2e31de441d4e592543c
Jack Zhou <377738226@qq.com>
2018-09-13 20:23:53 +0800

    move content to root component

--- src/app.js
+++ src/app.js
@@ -1,31 +1,4 @@
 import ReactDom from 'react-dom';
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { store } from 'models/store';
-import { getUser } from 'actions/auth';
-import Routes from './router';
-import './app.less';
+import HotApp from './root';
 
-// to enable module hot reload(instead of page reload)
-if (module.hot) {
-  module.hot.accept();
-}
-
-class App extends Component {
-  componentDidMount() {
-    // get user information after the app mounted(and check authentication status as well)
-    store.dispatch(getUser());
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="app">
-          <Routes />
-        </div>
-      </Provider>
-    );
-  }
-}
-
-ReactDom.render(<App store={store} />, document.getElementById('root'));
+ReactDom.render(<HotApp />, document.getElementById('root'));
